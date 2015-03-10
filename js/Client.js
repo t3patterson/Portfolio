59a6883c55b302c78610ac56308bd0a2a29a038c
Travis H <t3patterson@gmail.com>
2015-03-10 13:46:14 -0500

    navigation scroll animation added

--- js/Client.js
+++ js/Client.js
@@ -10,4 +10,29 @@
         $('.fixed-right-full').toggleClass('active');
     })
 
+    $('a[href*=#]:not([href=#])').on('click',function() {
+
+        //location.pathname takes everything after the hostname
+        if ( location.hostname == this.hostname 
+            ) {
+            
+            console.log('current-y-offset: '+$(this).offset().top)
+
+            //capture the hash-tag in the 
+            var target = $('[name=' + this.hash.slice(1) + ']');
+            
+            console.log('target-y-offset: '+target.offset().top)
+            //$(el).length > 1 determines that there is an element in the DOM
+            if (target.length) {
+                $('html,body').animate({
+                    //animate from the current position
+                    //   and set the target element's top y-offset as the scrollTop position to the 
+                    //      $('a[name='contact']). «return positionOfEl». «'top' property»
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
 })(typeof module === "object" ? module.exports : window);
